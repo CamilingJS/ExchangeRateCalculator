a14751668359fd7fb12ae89daf5156ccdf1fa23e
Jonathan <jonathancamiling@gmail.com>
2020-10-27 23:12:23 -0700

    dom elements selected

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const currencyEl_one = document.getElementById('current-one');
+const amountEl_one = document.getElementById('amount-one');
+
+const currencyEl_two = document.getElementById('current-two');
+const amountEl_two = document.getElementById('amount-two');
+
 function calculate() {
-  fetch('items.json')
-    .then((res) => res.json())
-    .then((data) => (document.body.innerHTML = data[0].text));
+ 
 }
 
 calculate();
