c2744a0cc54c0e3dfa3d6a562daa6cd5c6fa5312
Jonathan Camiling <JCamiling@employersadvantagedirect.com>
2021-01-03 21:01:24 -0800

    apikey hide attempt failed

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const currencyEl_one = document.getElementById('currency-one');
 const amountEl_one = document.getElementById('amount-one');
 const currencyEl_two = document.getElementById('currency-two');
 const amountEl_two = document.getElementById('amount-two');
-import API_KEY from "./apikey/apikey";
+
+const api_key = "e0e005aa6e13bbf2a80cb388";
 
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
@@ -12,7 +13,7 @@ function calculate() {
     const currency_one = currencyEl_one.value;
     const currency_two = currencyEl_two.value;
 
-    fetch(`https://v6.exchangerate-api.com/v6/e0e005aa6e13bbf2a80cb388/latest/${currency_one}`)
+    fetch(`https://v6.exchangerate-api.com/v6/${api_key}/latest/${currency_one}`)
         .then(res => res.json())
         .then(data => {
             console.log(data);
