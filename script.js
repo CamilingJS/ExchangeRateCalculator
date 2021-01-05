ead1def92fea64c2149454e9c7bfdc54548610b3
Jonathan <jonathancamiling@gmail.com>
2021-01-05 08:21:29 -0800

    script.js'

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-const currencyEl_one = document.getElementById('current-one');
+const currencyEl_one = document.getElementById('currency-one');
 const amountEl_one = document.getElementById('amount-one');
 
-const currencyEl_two = document.getElementById('current-two');
+const currencyEl_two = document.getElementById('currency-two');
 const amountEl_two = document.getElementById('amount-two');
 
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+//Fetch exchange rates and update the DOM
 function calculate() {
- 
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(
+    `https://v6.exchangerate-api.com/v6/e0e005aa6e13bbf2a80cb388/latest/${currency_one}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      //console.log(data);
+      const rate = data.rates[currency_two];
+
+      console.log(rate);
+    });
 }
+
+//Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
 
 calculate();
